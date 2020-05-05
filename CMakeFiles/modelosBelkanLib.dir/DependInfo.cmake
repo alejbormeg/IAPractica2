9d2d7933d816b9aff99222bb50afb1af204ff431
alejbormeg <alejbormeg@gmail.com>
2020-05-05 14:10:40 +0200

    Añado niveles 1 y 2

---
+++ CMakeFiles/modelosBelkanLib.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/aldeano/aldeano.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/aldeano.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/aldeano/brazo.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/brazo.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/aldeano/cabeza.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/cabeza.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/aldeano/cintura.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/cintura.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/aldeano/pierna.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/pierna.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/aldeano/torso.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/aldeano/torso.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/arbol/arbol.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/arbol/arbol.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/item/item.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/item/item.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/jugador/jugador3d.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/jugador/jugador3d.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/jugador/quesito.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/jugador/quesito.cpp.o"
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/src/modelos/retrato.cpp" "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/modelosBelkanLib.dir/src/modelos/retrato.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alejandro/Escritorio/3º DGIIM/2º Cuatrimestre/IA/Práctica2/practica2_linux/CMakeFiles/obj3DLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
